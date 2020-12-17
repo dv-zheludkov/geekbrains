3be60b9ba2856755b0caa58030d6417521d9bc6e
dv-zheludkov <dv@zheludkov.com>
2020-12-17 14:25:45 +0300

    Update homework_1_2.py

--- homework_1_2.py
+++ homework_1_2.py
@@ -1,9 +1,6 @@
 time = int(input('Введите время в секундах: '))
+hours = time // 3600
+min = time % 3600 // 60
 sec = time % 60
-min = time // 60
-hours = 0
-if min >= 60:
-    hours = min // 60
-    min = min % 60
 
 print(f'Вы ввели время в секундах {time}. В формате чч:мм:сс это будет {hours:02}:{min:02}:{sec:02}.')
